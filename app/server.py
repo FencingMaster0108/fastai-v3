2d00676c44ed86096bd90956212a01e976caa91a
FencingMaster0108 <62602743+FencingMaster0108@users.noreply.github.com>
2020-03-24 06:49:58 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1jV09hcS9-kl4za1A_yBi2aYkgfR1uDPo'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['ConorMcgregor', 'Khabib', 'NateDiaz', 'TonyFerguson']
 path = Path(__file__).parent
 
 app = Starlette()
